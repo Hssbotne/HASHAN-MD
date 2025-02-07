16ae0e6e8c9e47f0fd735f273f51aebffdca0f9a
Techdeployofc <Hashansathsara147@gmail.com>
2025-02-07 11:56:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qqgkHbKL#YdgFK6rfPpu_tfQF83tmabGu43i29NzhmVOqKftvO9s",
+SESSION_ID: process.env.SESSION_ID || "qMQ0GYCb#he66sivLKpSs9LDM8OO1KGl-FkSoyT9ydxNQ8LkParQ",
 };
